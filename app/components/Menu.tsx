761590f3f827b50cbbedae5e95550508c90dd979
Slyt4il <82972166+Slyt4il@users.noreply.github.com>
2023-11-15 16:52:06 +0700

    Add the ability to close menu by tapping outside

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -15,7 +15,7 @@ const Menu = () => {
         </div>
       </div>
       <>
-        <div className={show ? "fixed h-screen w-screen z-40 bg-black lg:hidden opacity-70 ease-out duration-500" : "fixed h-screen w-screen z-40 bg-black pointer-events-none lg:hidden opacity-0 ease-in duration-500"}></div>
+        <div className={show ? "fixed h-screen w-screen z-40 bg-black lg:hidden opacity-70 ease-out duration-500" : "fixed h-screen w-screen z-40 bg-black pointer-events-none lg:hidden opacity-0 ease-in duration-500"} onClick={() => setShow(!show)}></div>
         <div className={show ? "fixed z-50 bg-gradient-to-r from-pink-600 to-pink-500 text-slate-100 h-full top-24 left-0 w-2/3 lg:w-1/4 py-4 px-6 ease-out duration-500" : "fixed z-50 bg-gradient-to-r from-pink-600 to-pink-500 text-slate-100 h-full top-24 left-[-100%] w-2/3 lg:w-1/4 py-4 px-6 ease-in duration-500"}>
           <h2 className="text-2xl font-bold mt-8 mb-16">Rakchad Yingcheep</h2>
           <ul>
